994ef9fe64899f41d2215c1e0d9ac1dcac719bc7
Vitaemachina <jadissa@vitaemachina.com>
2014-07-20 22:10:53 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,1 @@
-function getConfig() {
-  
-  var config = new Object();
-  config.maxPlayers = 10;
-  config.minPlayers = 3;
-  config.handSize = 10;
-  
-  
-  return config;
-}
 
-exports.getConfig = getConfig;
